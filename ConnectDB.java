7a01a2b1a4b32830eecbe94cde2c7a36cb70a34d
Niraj <maharjanniraj87@gmail.com>
2024-04-22 18:12:49 +0545

    added database connection class

---
+++ ConnectDB.java
@@ -0,0 +1,24 @@
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+
+public class ConnectDB{
+    private static String USERNAME = "root";
+    private static String PASSWORD = "";
+    private static String URL = "jdbc:mysql://localhost:3306/carRental";
+
+    public static Connection getConnection() throws Exception{
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+    }
+    
+    public static void closeConnection(Connection connection){
+            if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
